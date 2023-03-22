c127551263c3e73d0650fa0fe7c8b0105664e3c9
Ken Irwin <ken@kenirwin.net>
2023-03-22 09:08:49 -0400

    weather poem works with fixed station/form

--- weatherPoem.js
+++ weatherPoem.js
@@ -1,3 +1,12 @@
+import { config } from 'dotenv';
+config();
+import { Configuration, OpenAIApi } from 'openai';
+const openAi = new OpenAIApi(
+  new Configuration({
+    apiKey: process.env.OPEN_AI_API_KEY,
+  })
+);
+
 import WeatherAPI from './models/Weather.js';
 const weatherAPI = new WeatherAPI();
 import dayjs from 'dayjs';
@@ -19,6 +28,21 @@ const today = dayjs().format('YYYY-MM-DD');
     'Think about the weather conditions given these data' +
     weatherStr +
     ' Write a sonnet about the weather. Do not refer to the numbers.';
-  console.log(weatherReq);
+  //   console.log(weatherReq);
   // convert weather data to human readable text string
+  let poem = await getResponse(weatherReq);
+  console.log(poem);
 })();
+
+async function getResponse(input) {
+  const response = await openAi.createChatCompletion({
+    model: 'gpt-3.5-turbo',
+    messages: [
+      {
+        role: 'user',
+        content: input,
+      },
+    ],
+  });
+  return response.data.choices[0].message.content;
+}
