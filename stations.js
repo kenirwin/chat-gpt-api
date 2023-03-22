62d0f506d04919c904596b68ebacdf5afe859442
Ken Irwin <ken@kenirwin.net>
2023-03-22 11:58:12 -0400

    added stations

--- stations.js
+++ stations.js
@@ -41,9 +41,91 @@ export default [
     },
   },
   {
+    id: '33345',
+    name: {
+      en: 'Kyiv, Ukraine',
+    },
+    country: 'UA',
+    region: 'KV',
+    identifiers: {
+      national: null,
+      wmo: '33345',
+      icao: 'UKKK',
+    },
+    location: {
+      latitude: 50.4,
+      longitude: 30.5667,
+      elevation: 166,
+    },
+    timezone: 'Europe/Kiev',
+    inventory: {
+      model: {
+        start: '2018-01-28',
+        end: '2023-03-30',
+      },
+      hourly: {
+        start: '1932-01-01',
+        end: '2023-03-21',
+      },
+      daily: {
+        start: '1881-01-01',
+        end: '2023-03-16',
+      },
+      monthly: {
+        start: 1881,
+        end: 2022,
+      },
+      normals: {
+        start: 1961,
+        end: 2020,
+      },
+    },
+  },
+  {
+    id: 'HKRE0',
+    name: {
+      en: 'Nairobi, Kenya',
+    },
+    country: 'KE',
+    region: 'NA',
+    identifiers: {
+      national: null,
+      wmo: null,
+      icao: 'HKRE',
+    },
+    location: {
+      latitude: -1.2773,
+      longitude: 36.8623,
+      elevation: 1626,
+    },
+    timezone: 'Africa/Nairobi',
+    inventory: {
+      model: {
+        start: null,
+        end: null,
+      },
+      hourly: {
+        start: '2001-07-29',
+        end: '2017-07-27',
+      },
+      daily: {
+        start: '2011-07-05',
+        end: '2015-06-13',
+      },
+      monthly: {
+        start: null,
+        end: null,
+      },
+      normals: {
+        start: 1961,
+        end: 1990,
+      },
+    },
+  },
+  {
     id: 'KOXD0',
     name: {
-      en: 'Oxford / McDonald',
+      en: 'Oxford, Ohio',
     },
     country: 'US',
     region: 'OH',
@@ -84,7 +166,7 @@ export default [
   {
     id: '47686',
     name: {
-      en: 'New Tokyo Inter-National Airport',
+      en: 'Tokyo, Japan',
     },
     country: 'JP',
     region: 'CH',
